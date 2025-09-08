1a8cd7e20ecbda451697f941ae3e01ec1e15b544
cookieshake <cookieshake.dev@gmail.com>
2025-09-08 22:31:35 +0900

    Refactor code structure for improved readability and maintainability

---
+++ template-python/app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class RequestBody(BaseModel):
+    name: str
+    good_name: str
+
+@app.get("/")
+def health():
+    return {"Hello": "World"}
+
+@app.post("/")
+def handle(data: RequestBody) -> dict:
+    return {"hello": f"{data.name} your good name is {data.good_name}"}
