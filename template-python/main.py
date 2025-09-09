3d99c0ae3a793fa5001d9f423ca5718da6b6d543
cookieshake <cookieshake.dev@gmail.com>
2025-09-09 19:03:24 +0900

    Remove empty README.md file from template-python directory

--- template-python/main.py
+++ template-python/main.py
@@ -14,3 +14,8 @@ def health():
 @app.post("/")
 def handle(data: RequestBody) -> dict:
     return {"hello": f"{data.name} your good name is {data.good_name}"}
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
